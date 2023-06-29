386d28eeecea4da7a5ae8cc5ef95f216eeec6e3f
Marco Drecktrah <marcodrecktrah@Marco.fritz.box>
2023-06-29 16:44:58 +0200

    saving current status - first draft of website without functionality

--- webapp-flask/app.py
+++ webapp-flask/app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html')
+
+@app.route("/output")
+def output():
+    return render_template('output.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
